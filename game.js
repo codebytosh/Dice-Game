8436d552cb687b8132592b427aeae276f8c98223
Shanthosh Pushparajah <spushpar@uwaterloo.com>
2018-04-04 14:33:10 -0400

    Initial Commit

---
+++ game.js
@@ -0,0 +1,10 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times they want. Each result gets added to his ROUND score
+- BUT, if the player rolls a '1', all his ROUND score gets deleted. After that, it's the next player's turn
+- The player can choose to 'Keep', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points wins the game
+
+*/
