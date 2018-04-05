41f4f340de120dc01a44fa60bc60ae2c7d107632
Shanthosh Pushparajah <spushpar@uwaterloo.com>
2018-04-04 21:50:43 -0400

    Create DOM Manuiplation and perpare Event Listner to roll dice

--- game.js
+++ game.js
@@ -8,3 +8,36 @@ GAME RULES:
 - The first player to reach 100 points wins the game
 
 */
+
+//Global Variables
+var scores, totalScore, activePlayer;
+
+scores = [0,0];
+totalScore = 0;
+activePlayer = 0;
+
+//Set initial scores to 0
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+//DOM
+
+//No dice image when page is first loaded - CSS
+document.querySelector(".dice").style.display = "none";
+
+//Event Listner -> When the the roll btn is clicked
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    
+    //Random Number Generator
+    var diceValue = Math.floor(Math.random() * 6) + 1;
+    
+    //Display the results -> Dice image
+    var diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + diceValue + ".png";
+    
+    //Update Scores
+    
+});
